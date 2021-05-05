ef7077d668ba24157ac1d8fa3373dda42378c69c
youman-cmd34 <83601176+youman-cmd34@users.noreply.github.com>
2021-05-05 14:15:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const generateBtn = document.querySelector(".generateBtn");
+
+generateBtn.addEventListener("click", generateRandomNum);
+
+function generateRandomNum() {
+  const fromNum = document.querySelector(".fromNum").value;
+  const toNum = document.querySelector(".toNum").value;
+
+  randomNum = Math.floor(Math.random() * parseInt(toNum));
+
+  if (randomNum >= parseInt(fromNum)) {
+    const randomNumEl = document.querySelector(".randomNumEl");
+    randomNumEl.innerHTML = randomNum;
+  }
+}
+if (window.getSelection) {
+    if (window.getSelection().empty) {  // Chrome
+      window.getSelection().empty();
+    } else if (window.getSelection().removeAllRanges) {  // Firefox
+      window.getSelection().removeAllRanges();
+    }
+  } else if (document.selection) {  // IE?
+    document.selection.empty();
+  }
